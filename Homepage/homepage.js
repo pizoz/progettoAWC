381b9afc72961f529989c46314b1b7a29e758f07
pizoz <116562586+pizoz@users.noreply.github.com>
2024-08-10 11:38:50 +0200

    Login/Logout temporaneo

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -18,6 +18,7 @@ async function immagini_carosello() {
 }
 
 async function crea_carosello() {
+    localStorage.setItem("logged", "true");
     let meals = await immagini_carosello();
     console.log(meals);
     console.log(Object.keys(meals))
